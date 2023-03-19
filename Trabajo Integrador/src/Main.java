248e74c62c837648917f2f3a2672b84c8ce432b9
Ignacio Scheffer <ignacioscheffer2004@gmail.com>
2023-03-19 19:07:33 -0300

    Update

--- Trabajo Integrador/src/Main.java
+++ Trabajo Integrador/src/Main.java
@@ -1,7 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+        System.out.println(Files.readAllLines(Paths.get("E:/AAA GitHub/TrabajoIntegrador/Trabajo Integrador/src/pronostico.csv")));
 
+    } catch(Exception e) {
+            System.out.println("Error: "+ e.getMessage());
+        }
     }
 }
