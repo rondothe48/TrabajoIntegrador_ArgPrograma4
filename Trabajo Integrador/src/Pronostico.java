da12d8d2450746865f5469ac5146d85c2494c882
rondothe48 <octirondoletto@gmail.com>
2023-03-23 12:30:39 -0300

    cambios

--- Trabajo Integrador/src/Pronostico.java
+++ Trabajo Integrador/src/Pronostico.java
@@ -4,9 +4,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Pronostico {
-    private Partido partido;
-    private Equipo equipo;
-    private ResultadoEnum resultado;
+
+    private int Mariana = 0;
+
+    private Equipo equipo1;
+    private Equipo equipo2;
+    private int golesEquipo1;
+    private int golesEquipo2;
+    static String[]ResultadoEnum = {"GANADOR", "EMPATE", "PERDEDOR"};
+    public int getGolesEquipo1() {
+        return golesEquipo1;
+    }
+
+    public void setGolesEquipo1(int golesEquipo1) {
+        this.golesEquipo1 = golesEquipo1;
+    }
+
+    public int getGolesEquipo2() {
+        return golesEquipo2;
+    }
+
+    public void setGolesEquipo2(int golesEquipo2) {
+        this.golesEquipo2 = golesEquipo2;
+    }
+
 
     /*public static void pronostico() throws IOException {
         Path archivoPronostico = Paths.get("C:/Nacho/GitHub Repositories/TrabajoIntegrador/Trabajo Integrador/src/pronostico.csv");
@@ -22,8 +43,19 @@ public class Pronostico {
 
     }*/
 
-    public int puntos() {
-        this.partido = new Partido(Equipo.ARGENTINA, Equipo.ARABIA_SAUDITA);
-        
+    public Pronostico(Equipo equipo1, Equipo equipo2) {
+        this.equipo1 = equipo1;
+        this.equipo2 = equipo2;
+        setGolesEquipo1(1);
+        setGolesEquipo2(2);
+        if (golesEquipo1 < golesEquipo2) {
+            System.out.println(equipo1 + "\n" + ResultadoEnum[0]);
+            System.out.println(equipo2 + "\n" + ResultadoEnum[2]);
+        } else {
+            System.out.println(equipo2 + "\n" + ResultadoEnum[0]);
+            System.out.println(equipo1 + "\n" + ResultadoEnum[2]);
+        }
     }
+
+    public void puntos()
 }
